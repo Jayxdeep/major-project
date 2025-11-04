db5403215d220d9bd9fd1c9bf3dd53eb17b26004
Jaydeep <jaydeepmukherjee2003@gmail.com>
2025-11-04 23:46:13 +0530

    fixed protocol issues

--- backend/services/dataServices.js
+++ backend/services/dataServices.js
@@ -1,10 +1,12 @@
 import SensorData from "../models/sensorModel.js";
 //testing and reading will do ? , aggr process and calc data [sql{grp by()},avg()]
-export const getavgmoist=async(hours=24)=>{
+export const getAvgMosit=async(hours=24)=>{
     const since=new Date(Date.now()-hours*60*60*1000);
     const data=await SensorData.aggregate([
-        {$match:{timestamp:{$gte:since}}},//sensor records frm 24hrs
-        {$group:{_id:null,avgMoisit:{$avg:"$moisture"}}} //grps filtered records
+        {$match:{timestamp:{$gte:since},$expr:{$ne:[{$toDouble:"$moisture"},null]}}},//sensor records frm 24hrs
+        {$addFields:{moistNum:{$convert:{input:"$moisture",to:"double",onError:null,onNull:null}}}},
+        {$group:{_id:null,avgMoisit:{$avg:"$moistNum"}}} //grps filtered records 
     ]);
+    console.log("aggr res",data);
     return data[0]?.avgMoisit || 0; 
 }
